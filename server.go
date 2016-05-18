78ff08ad159f6c22d469b5341a4623dcd2ec73d7
Omar Qazi <omar@bqe.com>
2016-05-18 16:56:56 -0700

    add dependencies

--- server.go
+++ server.go
@@ -10,10 +10,10 @@ var serverDirectory = flag.String("dir", ".", "Specify a filesystem path for the
 var serverPort = flag.String("port", ":8080", "Specify an address to serve at")
 
 func main() {
-	root := http.Dir(serverDirectory)
+	root := http.Dir(*serverDirectory)
 	http.Handle("/", http.FileServer(root))
 
-	if err := http.ListenAndServe(serverPort, nil); err != nil {
+	if err := http.ListenAndServe(*serverPort, nil); err != nil {
 		fmt.Println("Error starting server:", err)
 	}
 }
